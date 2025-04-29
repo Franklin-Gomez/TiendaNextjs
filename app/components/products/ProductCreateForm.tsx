374fbd086ffc2ddd5cade305d553e3713dc2fece
Franklin-Gomez <franklin_043097@hotmail.com>
2025-04-29 00:04:43 -0500

    add buttons for create product

---
+++ app/components/products/ProductCreateForm.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+export default function ProductSearchForm() {
+
+    const handleSearchForm = ( formData : FormData ) => { 
+        console.log("desde Crear Producto")
+        
+    }
+
+    return (
+        <form
+            action={handleSearchForm}
+            className=" flex items-center"
+        >
+
+            <input 
+                type="text" 
+                placeholder="Buscar Producto"
+                className="p-2 placeholder-gray-400 w-full"
+                name="search"
+            />
+
+            <input
+                type="submit"
+                className="bg-blue-600 p-2 uppercase text-white cursor-pointer"
+                value={'Buscar Producto'}
+            />
+
+        </form>
+    )
+}
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,14 +2,25 @@
 import React from 'react'
 import ProductTablet from '../components/products/ProductTablet'
 import ProductSearchForm from '../components/products/ProductSearchForm'
+import Link from 'next/link'
 
 export default function page() {
     return (
         <div>
 
             <h1 className='text-4xl text-center font-bold uppercase mb-3'> PC Master Race Productos </h1>
-            
-            <ProductSearchForm/>
+
+            <div className="flex flex-col lg:flex-row lg:justify-between gap-5">
+
+                <Link
+                href={'/products/new'}
+                className='bg-amber-300 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer'
+                > Crear Producto </Link>
+
+                <ProductSearchForm/>
+
+            </div>
+
             <ProductTablet/>
         </div>
     )
